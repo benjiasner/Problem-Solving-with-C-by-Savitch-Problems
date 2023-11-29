aa391554e418c871c5191c8b5de9a0e01c491c73
Benji Asner <54417348+benjiasner@users.noreply.github.com>
2023-11-28 22:55:50 -0600

    extraneous

---
+++ ch6_2.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+//write a program that will read a file of 
+// numbers of type int and output the frequency of each number in the file
+// - numbers are of type int
+// - file contains only whole numbers, 
+// positive and negative, separated by spaces, tabs or line breaks
+
+int main() {
+
+    // first define the variables we want
+    // then read the file
+    ifstream fin;
+
+    cout << "Begin editing files.\n";
+
+    fin.open("file2.dat");
+    if (fin.fail( )) {
+        cout << "Input file opening failed.\n";
+        exit(1);
+    }
+
+    char next;
+    int lineCounter;
+
+    cin.get(next);
+    while (! fin.eof( )){
+        if ( (next == '\n') || (next == '\r') ){
+        lineCounter++;
+        }
+    }
+
+    cout << "The total number of lines in this file is ";
+    cout << lineCounter;
+    cout << ".";
+
+    cout << "\n";
+    fin.close( );
+    cout << "Done reading file./n";
+
+    return 0;
+}
